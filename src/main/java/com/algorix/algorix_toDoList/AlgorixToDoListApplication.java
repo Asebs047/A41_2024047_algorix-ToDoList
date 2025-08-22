0074c89944d7cdebc81f39a8d5eca619ba97a7bc
Asebs047 <eherrera-2024047@kinal.edu.gt>
2025-08-22 16:17:23 -0600

    first commit

---
+++ src/main/java/com/algorix/algorix_toDoList/AlgorixToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.algorix.algorix_toDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorixToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorixToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorix/algorix_toDoList/AlgorixToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorix.algorix_toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorixToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
